47922b0008a69e42beec8587a9ae50acb04b5bc6
[clementgarcia38] <[clement.garcia@capgemini.com]>
2022-11-02 22:22:01 +0100

    nom unique azure queue storage...

--- service_queue.tf
+++ service_queue.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "storage_queue" {
-  name                     = "queuestorage"
+  name                     = "queuestoragecpa2"
   resource_group_name      = azurerm_resource_group.rg_hub.name
   location                 = local.location
   account_tier             = "Standard"
