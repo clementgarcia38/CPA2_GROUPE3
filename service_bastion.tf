5e115b2ccd2fdc18dbab5a0f5bdc543278404d02
[clementgarcia38] <[clement.garcia@capgemini.com]>
2022-11-02 21:47:30 +0100

    creation bastion

---
+++ service_bastion.tf
@@ -0,0 +1,26 @@
+resource "azurerm_bastion_host" "bastion_hub" {
+  name                = "bastionhub"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.rg_hub.name
+
+  ip_configuration {
+    name                 = "bastionip"
+    subnet_id            = azurerm_subnet.sub_bastion.id
+    public_ip_address_id = azurerm_public_ip.pip_bastion.id
+  }
+}
+
+resource "azurerm_public_ip" "pip_bastion" {
+  name                = "pipbastionhub"
+  location            = local.location
+  resource_group_name = azurerm_resource_group.rg_hub.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_subnet" "sub_bastion" {
+  name                 = "subbastion"
+  resource_group_name  = azurerm_resource_group.rg_hub.name
+  virtual_network_name = azurerm_virtual_network.network_hub.name
+  address_prefixes     = ["10.1.1.0/27"]
+}
