4a31d4179be5a831d0db970f1effa17369eac07d
[clementgarcia38] <[clement.garcia@capgemini.com]>
2022-11-02 22:17:11 +0100

    Ajout Queue

---
+++ service_queue.tf
@@ -0,0 +1,15 @@
+resource "azurerm_storage_account" "storage_queue" {
+  name                     = "queuestorage"
+  resource_group_name      = azurerm_resource_group.rg_hub.name
+  location                 = local.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  account_kind             = "StorageV2"
+  queue_properties {
+
+  }
+}
+resource "azurerm_storage_queue" "example" {
+  name                 = "queueformulaire"
+  storage_account_name = azurerm_storage_account.storage_queue.name
+}
