f5190d69fc540c4b30385562205a586078ef586f
[clementgarcia38] <[clement.garcia@capgemini.com]>
2022-11-02 20:45:43 +0100

    variables : location ajout

--- network_hub.tf
+++ network_hub.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "network_hub" {
   name                = "vnethub"
-  location            = azurerm_resource_group.rg_hub.location
+  location            = local.location
   resource_group_name = azurerm_resource_group.rg_hub.name
   address_space       = ["10.1.0.0/16"]
   #  dns_servers         = ["10.0.0.4", "10.0.0.5"]
--- rg_hub.tf
+++ rg_hub.tf
@@ -1,4 +1,4 @@
 resource "azurerm_resource_group" "rg_hub" {
   name     = "rghub"
-  location = "West Europe"
+  location = local.location
 }
