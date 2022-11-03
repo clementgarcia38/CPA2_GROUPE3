8e3062ecaff63f688ebbeeff168ad38057eea297
[clementgarcia38] <[clement.garcia@capgemini.com]>
2022-11-03 10:48:58 +0100

    modif subnet aks

--- service_aks.tf
+++ service_aks.tf
@@ -1,3 +1,11 @@
+
+  resource "azurerm_subnet" "sub_aks" {
+  name                 = "subaks"
+  resource_group_name  = azurerm_resource_group.rg_spoke.name
+  virtual_network_name = azurerm_virtual_network.network_spoke.name
+  address_prefixes     = ["10.2.1.0/24"]
+  }
+
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "akscpa2"
   location            = local.location
