cc7fae60e09f92ba6c038acae7da26ffeb228e46
[clementgarcia38] <[clement.garcia@capgemini.com]>
2022-11-04 11:15:11 +0100

    test container registry

--- service_registry.tf
+++ service_registry.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "container_registry" {
-  name                = "containerRegistry1"
+  name                = "containerRegistryCPA2"
   resource_group_name = azurerm_resource_group.rg_registry.name
   location            = local.location
   sku                 = "Standard"
